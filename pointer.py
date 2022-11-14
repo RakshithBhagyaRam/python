cde3e5759a98ed525e3700d65b5087012335ac8d
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-11-14 12:09:32 +0530

    pointer list
    def unpack

---
+++ pointer.py
@@ -0,0 +1,14 @@
+
+def unpack(p,q,r,s):
+	print(p+s)
+a=[2,3,4,5]
+unpack(*a)
+n=0
+a=[""]
+n=int(input('enter number of numbers '))
+print("enter numbers ")
+for i in range(0,n):
+	a[i]=input()
+#	a[i]+=ai]
+print(a[i])
+	
