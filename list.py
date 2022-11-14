8e4cabc19362c8bc35aaf2fc33d2293d6c99b342
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-11-14 12:15:26 +0530

    list append len

---
+++ list.py
@@ -0,0 +1,20 @@
+x=[1,2,3]
+y=[1,2,3]
+print(len(x)+len(y))
+number_list = []
+n = int(input("Enter the list size \n"))
+print("enter the values")
+for i in range(0, n):
+    item=input().split(" ")
+    number_list.append(item) the
+print(number_list)
+list1=['a',1]
+list2=list1
+list3=list2
+list2.append('b')
+print(list2,list3)
+
+print(len(list2+list1))
+t1=(1,2,3,4)
+t2=(4,2,3,1)
+print(t2>t1)
