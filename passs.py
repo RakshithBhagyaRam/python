ef3d33e99baa291dd135867eeff6d2b0f81d7ee2
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-01 00:01:11 +0530

    pass function
    if you don't have anything to write but you need a function
    
    then you can pass the function

---
+++ passs.py
@@ -0,0 +1,16 @@
+def pas:
+	pass
+
+#if you don't have anything to write but you need a def function
+
+
+for i in range(10):
+	pass
+
+#if you don't have anything to write but you need a for function
+
+
+while True:
+	pass
+
+#if you don't have anything to write but you need a while function
