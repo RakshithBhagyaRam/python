d1e6c02315494dd2d6357d269fd86d2bbe02e61b
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-02 23:55:26 +0530

    Taking input from user

---
+++ basics/input.py
@@ -0,0 +1,12 @@
+#taking string as input
+name = input("Enter your name : ")
+print("Your name is",name)
+
+#taking number as input 
+age = int(input("Enter your age : "))
+print("Your age is",age)
+
+#taking number with decimal point as input 
+salary = float(input("Enter your salary : "))
+print("Your salary is",salary)
+
