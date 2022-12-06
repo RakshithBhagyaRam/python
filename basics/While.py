70211a9d5318a71d434366d60866de0578bbfabb
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-06 23:12:28 +0530

    While loop

---
+++ basics/While.py
@@ -0,0 +1,10 @@
+n=10
+while(n>0):  #syntax : while (condition):
+  print(n)   #prints 10 to 1
+  n-=1
+  
+  
+  
+while(True): #while(Boolean condition):
+  print(n)   #it is a end less loop bcz the condition is always true
+  n+=1
