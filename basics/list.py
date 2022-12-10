a2ac9c784b05c2d32bb9b4b6083d6f5b325e3587
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-11 00:03:38 +0530

    list

--- basics/list.py
+++ basics/list.py
@@ -1,6 +1,6 @@
-# syntax :  variable_name = [list elements]
+# syntax :  variable_name = [ list_element_1 , list_element_2 .... list_element_n ]
 
-List=[] #declaring a List
-List2=[1,2,3,4]   # for numeric list elements
-List3=['abc','xyz','pqr']   # for string list elements
-print(List2)
+empty_list=[] #declaring a List
+list_for_numbers=[1,2,3,4]   # for numeric list, elements should be without quotes
+List3=['abc','xyz','pqr']   # for string list, elements should be with 'single' or "double" quotes
+print(List2) #printing list
