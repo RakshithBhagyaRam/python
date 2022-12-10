000a2274aaaa68811974ea4835481924252ab767
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-11 00:05:22 +0530

    list

--- basics/list.py
+++ basics/list.py
@@ -1,6 +1,7 @@
 # syntax :  variable_name = [ list_element_1 , list_element_2 .... list_element_n ]
 
-empty_list=[] #declaring a List
-list_for_numbers=[1,2,3,4]   # for numeric list, elements should be without quotes
+Empty_list=[] #declaring a List
+List_for_numbers=[1,2,3,4]   # for numeric list, elements should be without quotes
 List_for_strings=['abc','xyz','pqr']   # for string list, elements should be with 'single' or "double" quotes
-print(List2) #printing list
+
+print(List_for_numbers) #printing list
