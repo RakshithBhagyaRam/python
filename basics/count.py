e8a34653a24e255f6a67e5c0275a005c6c0ff14a
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-14 23:38:48 +0530

    count

--- basics/count.py
+++ basics/count.py
@@ -1,9 +1,10 @@
-vowel = 0
-cons=0
+if __name__ == '__main__':
+  vowel = 0
+  cons=0
 
-for i in a-z:
-  if i == 'a'or'e'or'i'or'o'or'u':
-    vowel+=1
-  else :
-    cons+=1
-print(f'{vowel} vowels and {cons} consonants' )
+  for i in range(ord(a),ord(z)+1):
+    if i == 'a'or'e'or'i'or'o'or'u':
+      vowel+=1
+    else :
+      cons+=1
+  print(f'{vowel} vowels and {cons} consonants' )
