cc175dbfa629862c789bda7c6c5c599c1cfb5add
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-16 22:51:43 +0530

    Count
    Counting vowels and consonants

--- basics/count.py
+++ basics/count.py
@@ -1,10 +1,10 @@
-import string
+import string 
 consonant =0
 vowel =0
-for letter in string.ascii_lowercase:
-  print(letter, end=" ")
-  if letter in ('a','e','i','o','u'):
-    vowel +=1
+for letter in string.ascii_lowercase:  #it consider on the lowercase characters 
+  print(letter, end=" ") #print every alphabet
+  if letter in ('a','e','i','o','u'): #check whether the perticular alphabet in this tuple
+    vowel +=1  #if yes vowels increase by 1 count
   else :
-    consonant +=1
+    consonant +=1 #else consonants increase by 1 count
 print(f'{vowel} vowels and {consonant} consonants' )
