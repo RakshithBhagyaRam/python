03bf5404f4f5b7fc5b68d285bbccefacc32e9b7f
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-16 23:16:20 +0530

    Variable
    Rules to declare variables

---
+++ basics/variables.py
@@ -0,0 +1,16 @@
+'''
+rules to be followed to declare a variable
+
+1. variable names should not be a keyword (predefined word)
+2. should not start witha a number
+3. should not consider special characters like (:;-&+#@)  except under_score " _ "
+4. keywords can be used as a variable name only if capitalise the word . because python is a case sensitive language 
+
+'''
+
+name = "Raju" #name is a variable that consist of words or sentenses with in "double" or 'single' quotes
+
+age = 25  #age is a variable that consist of integer (numerical) value 
+
+salary = 40000.00 # salary is a variable that consists of floating point value
+#floating point means number forward by a decimal values
