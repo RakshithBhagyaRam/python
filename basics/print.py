454fac25e5af637f4c6047a4259f51be58c25005
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-16 23:01:52 +0530

    Print

--- basics/print.py
+++ basics/print.py
@@ -5,3 +5,15 @@ print(' 123 ') #output :- 123
 
 
 print('Hello, world!')  # This program prints Hello, world! 
+
+
+name = "laavu"  # variable name consist of string
+age = 20        # variable age consist of integer (number)
+
+print (f'my name is {name} my age is {age}')  
+# syntax :- print(f'message to be displayed {variable name} ')
+
+# f means formated string literals 
+
+
+
