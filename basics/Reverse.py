004640facc2177ed1b1904cfe08d7b21c2f103a5
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-20 23:16:49 +0530

    Reverse number
    1. Using slicing
    2. Using loop

---
+++ basics/Reverse.py
@@ -0,0 +1,17 @@
+# Reversing a number using slicing 
+
+
+n = int(input("Enter a number:"))
+print(str(n)[::-1])
+
+
+# Reversing a number using loop 
+
+n = int(input("Enter a number:"))
+reversed_number =0
+while n!=0:
+    num = n%10
+    reversed_number = reversed_number * 10 + num 
+    n=n//10
+    print(reversed_number, end='')
+
