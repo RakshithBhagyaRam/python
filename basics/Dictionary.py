520021afd249e13f1011552006eae7087678f831
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-21 22:49:27 +0530

    Dictionary
    Syntax and rules to declare dictionaries

--- basics/Dictionary.py
+++ basics/Dictionary.py
@@ -1,5 +1,12 @@
-# Dictionaries in Python
+# Dictionaries in Python 
+
 # syntax  variable_name = { key(1) : value(1), key(2) : value(2) ,....... , key(n) : value(n) } 
+
+# note : string should always be in 'single' or "double" quotes 
+#        integer can be written without quotes 
+#        one key should have only one value 
+#        key and value is separated by colon(:) 
+#        key-value pair is separated by coma(,) 
 
 dict= {'Name':'Ram',     
 
