d79e358dac36219bb86947047ecc85e41578f42d
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-21 22:41:49 +0530

    Dictionary

---
+++ basics/Dictionary.py
@@ -0,0 +1,11 @@
+# Dictionaries in Python
+
+dict= {'Name':'Ram',     
+
+       'Age':50,
+
+       'Gender':'Male'}
+
+print (dict)     #prints all the keys and values of the dictionary 
+
+print (dict['Age'])  #prints only the value of that perticular key 'Age' 
