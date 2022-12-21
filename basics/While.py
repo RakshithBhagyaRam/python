bb529057286acf001b601604c8fd3ab163b4c61a
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-21 22:25:37 +0530

    While

--- basics/While.py
+++ basics/While.py
@@ -3,6 +3,13 @@ while(n>0):  #syntax : while (condition):
   print(n)   #prints 10 to 1
   n-=1
   
+
+n=1
+while(n<=10):  #syntax : while (condition):
+  print(n)   #prints 1 to 10
+  n+=1
+  
+  
   
   
 while(True): #while(Boolean condition):
