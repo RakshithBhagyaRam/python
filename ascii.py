99564389e801f3b5ff82c6216dba554eef7b8da3
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-24 21:15:40 +0530

    ASCII triangle

--- ascii.py
+++ ascii.py
@@ -1,7 +1,7 @@
 n=11
-ascii=65
+ascii=65      #ascii value for A
 for i in range (n):
-     print((n-i-1)*" ", end="")
+     print((n-i-1)*" ", end="")  #creates a triangular shape
      for j in range (0,i+1):
      	print(chr(ascii),end=" ")
      	ascii+=1
